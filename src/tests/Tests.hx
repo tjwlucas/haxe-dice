66a6e7d7ae430db6b9fc1852640ad4241ae41c6b
Thomas Lucas <tom@thomaslucas.co.uk>
2021-03-25 15:19:03 +0000

    Add failure exit code for nodejs

--- src/tests/Tests.hx
+++ src/tests/Tests.hx
@@ -3,7 +3,7 @@ package tests;
 import utest.Runner;
 
 class Tests {
-	public static function main() {		
+	public static function main() {
 		var runner = new Runner();
 		new NoExitReport(runner);
 		runner.addCases(tests.cases);
@@ -15,16 +15,18 @@ class NoExitReport extends utest.ui.text.PrintReport {
 	override function complete(result:utest.ui.common.PackageResult) {
 		this.result = result;
 
-    #if js
-      js.html.Console.log(this.getResults());
-    #elseif sys
-		  Sys.println(this.getResults());
-    #end
-
-    #if sys
+		#if js
+		js.html.Console.log(this.getResults());
+		#elseif sys
+		Sys.println(this.getResults());
+		#end
+		
 		if (!result.stats.isOk) {
+			#if sys
 			Sys.exit(1);
+			#elseif js
+			js.Syntax.code('process.exit(1)');
+			#end
 		}
-    #end
 	}
 }
