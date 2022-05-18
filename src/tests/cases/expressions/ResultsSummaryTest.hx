6127217c9f25d535cfed65fa1c2077786a078624
Thomas Lucas <tom@thomaslucas.co.uk>
2022-05-18 11:07:04 +0000

    Add tests for isNumeric and isInteger properties

--- src/tests/cases/expressions/ResultsSummaryTest.hx
+++ src/tests/cases/expressions/ResultsSummaryTest.hx
@@ -28,7 +28,7 @@ class ResultsSummaryTest extends Test {
         );
     }
 
-    function specIsNumeric() {
+    function specIsNumericAndInteger() {
         generator.mockResults[6] = [2, 3, 1, 4, 3, 6, 2, 4, 1, 5, 6, 2];
         var expression = manager.getComplexExpression("2d6 + 3");
         for (i in 0...6) {
@@ -36,6 +36,29 @@ class ResultsSummaryTest extends Test {
         }
         var summary = expression.resultsSummary;
         Assert.isTrue(summary.isNumeric);
+
+        var expression = manager.getComplexExpression("'string'");
+        expression.roll();
+        var summary = expression.resultsSummary;
+        Assert.isFalse(summary.isNumeric);
+
+        generator.mockResults[4] = [1, 2, 3, 4];
+        var expression = manager.getComplexExpressionRuntime("[3, 23, 4.5, true][d4 - 1]");
+        for (i in 0...2) {
+            expression.roll();
+        }
+        var summary = expression.resultsSummary;
+        // [3, 23] - All integers, so far
+        Assert.isTrue(summary.isNumeric);
+        Assert.isTrue(summary.isInteger);
+        expression.roll();
+        // [3, 23, 4.5] - Now, there's a Float, still numeric, but not integer
+        Assert.isTrue(summary.isNumeric);
+        Assert.isFalse(summary.isInteger);
+        expression.roll();
+        // [3, 23, 4.5, true] - Bool stops it being numeric, at all
+        Assert.isFalse(summary.isNumeric);
+        Assert.isFalse(summary.isInteger);
     }
 
     function specResultsSummaryMap() {
